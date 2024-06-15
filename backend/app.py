8cf08951848198f7788d5b3706f8572ce9c505c1
Anand Krishna <anand.krishna0802@gmail.com>
2024-06-15 19:25:58 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+script_dir = Path(__file__).resolve().parent
+genre_embeddings_path = script_dir / 'genre_embeddings.json'
+model_architecture_path = script_dir / 'MGR Model/model_architecture.json'
+model_weights_path = script_dir / 'MGR Model/model_weights.weights.h5'
+
+with open(genre_embeddings_path, 'r') as json_file:
+    genre_embeddings = json.load(json_file)
+
+with open(model_architecture_path, 'r') as json_file:
+    model_json = json_file.read()
+
+model = tf.keras.models.model_from_json(model_json)
+model.load_weights(model_weights_path)
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+@app.route('/predict_genres', methods=['POST'])
+
+def predict_genres():
+    try:
+        user_input = request.json
+
+        features = list(user_input.values())
+        input_features = np.array(features).reshape(1,-1)
+
+
+        predictions = model.predict(input_features)[0]
+
+        genre_embeddings_values = np.array(list(genre_embeddings.values()))
+
+
+        similarities = cosine_similarity([predictions], genre_embeddings_values)[0]
+
+        closest_indices = np.argsort(similarities)[-4:][::-1]
+
+        closest_genres = [list(genre_embeddings.keys())[i] for i in closest_indices]
+
+
+        return jsonify({'predictedGenres': closest_genres})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+app.run(debug=True, host='0.0.0.0',port=8000)
---
+++ backend/index.js
@@ -0,0 +1,148 @@
+import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import bodyParser from 'body-parser';
+dotenv.config()
+
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+app.listen(5000,'0.0.0.0', function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT : 5000");
+});
+
+
+// Function to get Spotify access token
+async function getAccessToken() {
+    const authOptions = {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'client_credentials'
+      })
+    };
+  
+    try {
+      const response = await fetch('https://accounts.spotify.com/api/token', authOptions);
+      const data = await response.json();
+  
+      if (response.ok) {
+        return data.access_token;
+      } else {
+        console.error('Error fetching access token:', data);
+        return null;
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+
+
+// songSearchAPI - SpotifySearchResults
+app.post('/songSearch', async (req,res) => {
+    const {query} = req.body
+    
+    console.log(query)
+    console.log(encodeURIComponent(query))
+
+    try{
+        const accessToken = await getAccessToken();
+        const spotifySearchUrl = `https://api.spotify.com/v1/search?type=track&q=${encodeURIComponent(query)}`;
+        const spotifySearchOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        }
+
+        const SpotifySearchAPI_res = await fetch(spotifySearchUrl, spotifySearchOptions)
+        const data = await SpotifySearchAPI_res.json()
+
+        res.json(data)
+
+    } catch(error) {
+        console.error('Error searching Spotify: ', error)
+        res.status(500).json({error: 'Failed to search Spotify'})
+    }
+});
+
+// songNameAPI - trueGenre
+app.post('/songName', async (req,res) => {
+    const {artistId, trackId} = req.body
+    
+    try{
+
+        const accessToken = await getAccessToken();
+        const spotifyArtistUrl = `https://api.spotify.com/v1/artists/${encodeURIComponent(artistId)}`
+        const spotifyArtistOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        }
+
+        const SpotifyArtistAPI_res = await fetch(spotifyArtistUrl, spotifyArtistOptions)
+        const SpotifyArtistAPI_data = await SpotifyArtistAPI_res.json()
+        const trueGenres = await SpotifyArtistAPI_data.genres
+        console.log(trueGenres)
+
+
+
+        const spotifyAudioFeaturesUrl = `https://api.spotify.com/v1/audio-features/${encodeURIComponent(trackId)}`
+        const spotifyAudioFeaturesOptions = {
+          method: 'GET',
+          headers: { 
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+
+        const SpotifyAudioFeaturesAPI_res = await fetch(spotifyAudioFeaturesUrl,spotifyAudioFeaturesOptions)
+        const SpotifyAudioFeaturesAPI_data = await SpotifyAudioFeaturesAPI_res.json()
+        const AudioFeatures = {
+          duration_ms: SpotifyAudioFeaturesAPI_data.duration_ms,
+          danceability: SpotifyAudioFeaturesAPI_data.danceability,
+          energy: SpotifyAudioFeaturesAPI_data.energy,
+          key: SpotifyAudioFeaturesAPI_data.key,
+          loudness: SpotifyAudioFeaturesAPI_data.loudness,
+          mode: SpotifyAudioFeaturesAPI_data.mode,
+          speechiness: SpotifyAudioFeaturesAPI_data.speechiness,
+          acousticness: SpotifyAudioFeaturesAPI_data.acousticness,
+          instrumentalness: SpotifyAudioFeaturesAPI_data.instrumentalness,
+          liveness: SpotifyAudioFeaturesAPI_data.liveness,
+          valence: SpotifyAudioFeaturesAPI_data.valence,
+          tempo: SpotifyAudioFeaturesAPI_data.tempo,
+          time_signature: SpotifyAudioFeaturesAPI_data.time_signature
+        }
+
+        console.log(AudioFeatures)
+
+        const flaskServer = process.env.REACT_APP_BACKEND_SERVER_URL
+
+        const predictionRes = await fetch(`${flaskServer}:8000/predict_genres`,{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(AudioFeatures)
+        })
+        const prediction = await predictionRes.json()
+        const predictedGenres = prediction.predictedGenres
+
+        res.json({
+            T_genre: trueGenres,
+            P_genre: predictedGenres
+        })
+
+    }catch(error){
+        console.error('Error occurred: ', error)
+    }
+})
